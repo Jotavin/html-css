98ab23b9f3eff058daf83e3212286497b6ede982
João Victor Sena Salomão <jv.sena0@gmail.com>
2022-11-08 13:09:44 -0300

    Update email.php

--- exercicios/Formulário.html/email.php
+++ exercicios/Formulário.html/email.php
@@ -8,8 +8,8 @@ $mensagem = addslashes($_POST['message']);
 
 $to = "jv.sena0@gmail.com";
 $subject = "Contato - Programador @Jotavin";
-$body = "Nome: ".$nome. "\r\n"
-        "Email: ".$email. "\r\n"
+$body = "Nome: ".$nome. "\r\n".
+        "Email: ".$email. "\r\n".
         "Mensagem: ".$mensagem;
 $header = "From:jv.sena00@gmail.com"."\r\n"."Reply-To:".$email."\r\n"."X=Mailer:PHP/".phpversion()
 
