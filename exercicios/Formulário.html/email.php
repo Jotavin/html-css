95d07048ebcfeb11bf89e3c9fa1ba4fe97142f05
João Victor Sena Salomão <jv.sena0@gmail.com>
2022-11-08 13:06:49 -0300

    Formulário com php

---
+++ exercicios/Formulário.html/email.php
@@ -0,0 +1,24 @@
+<?php
+
+if(isset($_POST['email']) && !empty($_POST['email'])){
+
+$nome = addslashes($_POST['name']);
+$email = addslashes($_POST['email']);
+$mensagem = addslashes($_POST['message']);
+
+$to = "jv.sena0@gmail.com";
+$subject = "Contato - Programador @Jotavin";
+$body = "Nome: ".$nome. "\r\n"
+        "Email: ".$email. "\r\n"
+        "Mensagem: ".$mensagem;
+$header = "From:jv.sena00@gmail.com"."\r\n"."Reply-To:".$email."\r\n"."X=Mailer:PHP/".phpversion()
+
+if(mail($to,$subject,$body,$header)){
+
+    echo("Email enviado com sucesso!");
+}else{
+    echo("O email não pôde ser enviado.");
+}
+
+}
+?>
